dba8175dbfbc4eed958657c4b521ab9deff281bb
Evlogi Hristov <evlogihristov@yahoo.com>
2013-05-11 18:13:15 +0300

    Made a class GameField and placed everything that has to do with the game field in it.

--- Game-Fifteen-6/Game15.cs
+++ Game-Fifteen-6/Game15.cs
@@ -7,9 +7,7 @@
 
     public class Game15
     {
-        private static readonly int[,] field = new int[4, 4];
         private static readonly OrderedMultiDictionary<int, string> bestResults = new OrderedMultiDictionary<int, string>(true);
-        private static readonly Dictionary<int, Coordinates> numberPositions = new Dictionary<int, Coordinates>();
 
         public static void Main()
         {
@@ -21,34 +19,17 @@
         {
             do
             {
-                RandomGameField();
+                GameField.RandomField();
             }
-            while (IsGameFieldSolved());
+            while (GameField.IsSolved());
 
             Console.WriteLine("Welcome to the game “15”. Please try to arrange the numbers sequentially." +
                 "Use 'top' to view the top scoreboard, 'restart' to start a new game and 'exit' to quit the game.");
         }
 
-        private static void RandomGameField()
-        {
-            List<int> numbers = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
-            numberPositions.Clear();
-            Random rand = new Random();
-            for (int i = 0; i < field.GetLength(0); i++)
-            {
-                for (int j = 0; j < field.GetLength(1); j++)
-                {
-                    int position = rand.Next(0, numbers.Count);
-                    field[i, j] = numbers[position];
-                    numberPositions.Add(numbers[position], new Coordinates(i, j));
-                    numbers.RemoveAt(position);
-                }
-            }
-        }
-
         private static void StartGame()
         {
-            PrintGameField();
+            GameField.Print();
             Console.Write("Enter a number to move: ");
             string input = Console.ReadLine();
             bool gameIsFinished = false;
@@ -70,7 +51,7 @@
                         {
                             if (0 < numberToMove && numberToMove < 16)
                             {
-                                TryToMoveNumber(numberToMove);
+                                GameField.TryToMoveNumber(numberToMove);
                             }
                             else
                             {
@@ -85,8 +66,8 @@
                         break;
                 }
 
-                PrintGameField();
-                gameIsFinished = IsGameFieldSolved();
+                GameField.Print();
+                gameIsFinished = GameField.IsSolved();
                 if (gameIsFinished)
                 {
                     AddToScoreBoard(moves);
@@ -99,51 +80,12 @@
             }
         }
 
-        private static void TryToMoveNumber(int numberToMove)
-        {
-            if (numberPositions[0].CheckNeighbour(numberPositions[numberToMove]))
-            {
-                Coordinates temp = numberPositions[0];
-                numberPositions[0] = numberPositions[numberToMove];
-                numberPositions[numberToMove] = temp;
-                field[numberPositions[numberToMove].Row, numberPositions[numberToMove].Col] = numberToMove;
-                field[numberPositions[0].Row, numberPositions[0].Col] = 0;
-            }
-            else
-            {
-                Console.WriteLine("Illegal command!");
-            }
-        }
-
         private static void AddToScoreBoard(int moves)
         {
             Console.WriteLine("Congratulations! You won the game in {0} moves.", moves);
             Console.Write("Please enter your name for the top scoreboard: ");
             string name = Console.ReadLine();
             bestResults.Add(moves, name);
-        }
-
-        private static bool IsGameFieldSolved()
-        {
-            int[,] matrix =
-            {
-                { 1, 2, 3, 4 },
-                { 5, 6, 7, 8 },
-                { 9, 10, 11, 12 },
-                { 13, 14, 15, 0 }
-            };
-            for (int row = 0; row < field.GetLength(0); row++)
-            {
-                for (int col = 0; col < field.GetLength(1); col++)
-                {
-                    if (field[row, col] != matrix[row, col])
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
         }
 
         private static void PrintBestOfTheBest()
@@ -159,29 +101,6 @@
                 Console.WriteLine("{0}. {1} --> {2} moves", counter, item.Value, item.Key);
                 counter++;
             }
-        }
-
-        private static void PrintGameField()
-        {
-            Console.WriteLine(" -------------------");
-            for (int i = 0; i < field.GetLength(0); i++)
-            {
-                Console.Write("|");
-                for (int j = 0; j < field.GetLength(1); j++)
-                {
-                    if (field[i, j] == 0)
-                    {
-                        Console.Write("    |");
-                        continue;
-                    }
-
-                    Console.Write(" {0,2} |", field[i, j]);
-                }
-
-                Console.WriteLine();
-            }
-
-            Console.WriteLine(" -------------------");
         }
     }
 }
---
+++ Game-Fifteen-6/GameField.cs
@@ -0,0 +1,91 @@
+﻿namespace Game15
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class GameField
+    {
+        public static readonly int[,] field = new int[4, 4];
+        public static readonly Dictionary<int, Coordinates> numberPositions = new Dictionary<int, Coordinates>();
+
+        public static void Print()
+        {
+            Console.WriteLine(" -------------------");
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+                    if (field[i, j] == 0)
+                    {
+                        Console.Write("    |");
+                        continue;
+                    }
+
+                    Console.Write(" {0,2} |", field[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine(" -------------------");
+        }
+        
+        public static bool IsSolved()
+        {
+            int[,] matrix =
+            {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 0 }
+            };
+            for (int row = 0; row < GameField.field.GetLength(0); row++)
+            {
+                for (int col = 0; col < GameField.field.GetLength(1); col++)
+                {
+                    if (GameField.field[row, col] != matrix[row, col])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public static void RandomField()
+        {
+            List<int> numbers = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+            numberPositions.Clear();
+            Random rand = new Random();
+            for (int i = 0; i < GameField.field.GetLength(0); i++)
+            {
+                for (int j = 0; j < GameField.field.GetLength(1); j++)
+                {
+                    int position = rand.Next(0, numbers.Count);
+                    GameField.field[i, j] = numbers[position];
+                    numberPositions.Add(numbers[position], new Coordinates(i, j));
+                    numbers.RemoveAt(position);
+                }
+            }
+        }
+
+        public static void TryToMoveNumber(int numberToMove)
+        {
+            if (numberPositions[0].CheckNeighbour(numberPositions[numberToMove]))
+            {
+                Coordinates temp = numberPositions[0];
+                numberPositions[0] = numberPositions[numberToMove];
+                numberPositions[numberToMove] = temp;
+                field[numberPositions[numberToMove].Row, numberPositions[numberToMove].Col] = numberToMove;
+                field[numberPositions[0].Row, numberPositions[0].Col] = 0;
+            }
+            else
+            {
+                Console.WriteLine("Illegal command!");
+            }
+        }
+    }
+}

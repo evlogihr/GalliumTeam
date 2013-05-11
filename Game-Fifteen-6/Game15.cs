804ee09be296f13cedf84969f9fb941b0b179a9b
Evlogi Hristov <evlogihristov@yahoo.com>
2013-05-11 19:00:15 +0300

    Trying to merge the changes we made.

--- Game-Fifteen-6/Game15.cs
+++ Game-Fifteen-6/Game15.cs
@@ -2,19 +2,13 @@
 {
     using System;
     using System.Linq;
-    using Wintellect.PowerCollections;
 
     public class Game15
     {
-        
-
         public static void Main()
         {
             GameManeger.Initialize();
             GameManeger.StartGame();
-        }
-
-            }
         }
     }
 }

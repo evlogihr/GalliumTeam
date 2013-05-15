2e9b83d0677ca82b49c7772ac65d8c44c4276242
sashomaga <saswork@abv.bg>
2013-05-15 11:11:20 +0300

    Communicator test

---
+++ GameTest/ComunicatorTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Game15;
+using System.IO;
+
+namespace GameTest
+{
+    [TestClass]
+    public class ComunicatorTest
+    {
+        [TestMethod]
+        public void TestGetNumber()
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+
+                Console.SetOut(sw);
+
+
+
+                using (StringReader sr = new StringReader(string.Format("6{0}", Environment.NewLine)))
+                {
+                    
+                    Console.SetIn(sr);
+
+                    string result = Communicator.GetNumber();
+
+
+
+
+
+                    string expected = "5";//string.Format("Enter a number to move: {0}", Environment.NewLine);
+
+                    Assert.AreEqual<string>(expected, result, "expected {0}", expected);
+
+                }
+
+            }
+        }
+    }
+}

88f66f48a41dd33f5225c3e53994cc51757db1b8
BobbyBorisov <borislav.borisov.tues@gmail.com>
2013-05-16 23:38:01 +0300

    Added Tests for Player

---
+++ GameTest/PlayerTests.cs
@@ -0,0 +1,24 @@
+﻿namespace GameTest
+{
+    using System;
+    using Game15;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PlayerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestPlayerNullForName()
+        {
+            IPlayer player = new Player(null, 10);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestPlayerNegativeScore()
+        {
+            IPlayer player = new Player("Pesho", -100);   
+        }
+    }
+}
